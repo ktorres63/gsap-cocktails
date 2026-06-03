d5c3b1346737ee9a3204e38a85bf728f9ed3361d
ktorres63 <ktorresaro@unsa.edu.pe>
2026-06-02 23:08:36 -0500

    Add assets to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger, SplitText);
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="flex-center h-screen">
-      <h1 className="text-7xl text-indigo-300">Hello Gsap !!</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { navLinks } from "../../constants";
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="">
+        <a href="#home" className="flex items-center gap-2">
+          <img src="/images/logo.png" alt="logo" />
+          <p>Velvet Pour</p>
+        </a>
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
