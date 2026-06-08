81ea7ff3e2a584c7e499c2b657810b4aea823530
ktorres63 <ktorresaro@unsa.edu.pe>
2026-06-07 21:44:54 -0500

    Implemet the first part of hero section"

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black"></div>
     </main>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,27 @@
-import React from "react";
 import { navLinks } from "../../constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 function Navbar() {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "rgba(0, 0, 0, 0.8)",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      },
+    );
+  });
   return (
     <nav>
       <div className="">
