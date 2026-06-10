2fbdefa2538a12d68cb52b77593dd289afc58708
ktorres63 <ktorresaro@unsa.edu.pe>
2026-06-09 22:34:02 -0500

    Implement the art section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
+import Art from "./components/Art";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
     </main>
   );
 }
