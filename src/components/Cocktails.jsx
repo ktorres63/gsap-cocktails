7b54087c97c8119c59ae8890ab4477c06773ce68
ktorres63 <ktorresaro@unsa.edu.pe>
2026-06-09 21:30:24 -0500

    Update Cocktails

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -1,7 +1,5 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { useRef } from "react";
-import { useMediaQuery } from "react-responsive";
 import { cocktailLists, mockTailLists } from "../../constants";
 
 function Cocktails() {
