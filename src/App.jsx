e767ee59be1e85ea6b5c15078ff650247fed2b04
ktorres63 <ktorresaro@unsa.edu.pe>
2026-06-09 22:04:24 -0500

    Implement the About Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   );
 }
